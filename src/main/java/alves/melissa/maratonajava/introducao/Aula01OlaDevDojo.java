9ea9b61e3fdc220f0fe8312f0b47f30636787852
melissaoalves <msnmelissaoa15@hotmail.com>
2025-07-23 22:05:57 -0300

    feat: add java primitive types examples

---
+++ src/main/java/alves/melissa/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package alves.melissa.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/alves/melissa/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package alves.melissa.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long, boolean
+        int age = 22;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        char character = 'A';
+        byte ageByte = -128;
+        short ageShort = 22;
+        long ageLong = 22;
+        boolean trueBoolean = true;
+        boolean falseBoolean = false;
+
+        System.out.println("I'm " +age+ " years old.");
+        System.out.println(trueBoolean);
+    }
+}
